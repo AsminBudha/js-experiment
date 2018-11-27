821d75c670120900ad9fc65c96c4a0b2a69ee921
asmin <sminmgr@gmail.com>
2018-11-27 12:23:10 +0545

    Fallten Recursive Code

--- excersise/flattenarray.js
+++ excersise/flattenarray.js
@@ -16,3 +16,36 @@ var output=input.reduce(function(acc,val){
 },[]);
 
 console.log(output);
+
+var inputN=[[1,[7,8],3],[2,4,5],6];
+
+function traverse(acc,val){
+	if(Array.isArray(val)){
+		for(var i=0;i<val.length;i++){
+			// console.log(val[i]);
+			if(acc.indexOf(val[i])==-1)
+				acc=traverse(acc,val[i]);
+		}
+	}
+	else{
+		// console.log('Checked');
+		// console.log(acc);
+		if(acc.indexOf(val)==-1)
+			acc.push(val);
+	}
+
+	return acc;
+}
+
+var outputN=inputN.reduce(function(acc,val){
+	if(Array.isArray(val)){
+		acc=traverse(acc,val);
+	}
+	else{
+		if(acc.indexOf(val)==-1)
+			acc.push(val);
+	}
+	return acc;
+},[]);
+
+console.log(outputN);
