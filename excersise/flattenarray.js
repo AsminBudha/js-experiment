0d40ccfd92088797494fe20dda4e6a167d3005e5
asmin <sminmgr@gmail.com>
2018-11-27 10:11:23 +0545

    Flatten Duplication removed

--- excersise/flattenarray.js
+++ excersise/flattenarray.js
@@ -3,11 +3,14 @@ var input=[[1,2,3],[2,4,5],6];
 var output=input.reduce(function(acc,val){
 	if(Array.isArray(val)){
 		for(var i=0;i<val.length;i++){
-			acc.push(val[i]);
+			// console.log(val[i]);
+			if(acc.indexOf(val[i])==-1)
+				acc.push(val[i]);
 		}
 	}
 	else{
-		acc.push(val);
+		if(acc.indexOf(val)==-1)
+			acc.push(val);
 	}
 	return acc;
 },[]);
