3f9a30094d58d216f675d9916e342fe2309a5485
asmin <sminmgr@gmail.com>
2018-11-30 19:08:33 +0545

    Flappy Bird completed with sprite and images

--- ast3/script.js
+++ ast3/script.js
@@ -1,4 +1,4 @@
-const GAP=100;
+const GAP=150;
 const CONTAINER_HEIGHT=600;
 const CONTAINER_WIDTH=1300;
 
@@ -10,8 +10,8 @@ class Bird{
 	constructor(){
 		this.x=100;
 		this.y=300;
-		this.height=10;
-		this.width=10;
+		this.height=35;
+		this.width=44;
 		this.down=1;
 		birdTHAT=this;
 
@@ -27,42 +27,50 @@ class Bird{
 		this.elem.style.top=this.y+'px';
 		this.elem.style.left=this.x+'px';
 		this.elem.style.zIndex='2';
+		this.elem.style.background='url(\'bird.png\') 0px 0px';
+
 
 		document.getElementById('container').appendChild(this.elem);
 	}
 
 	update(){
 		this.y+=this.down;
-		// let x=parseInt(this.elem.style.left)+1;
-
 		this.elem.style.top=this.y+'px';
-		// this.elem.style.left=x+'px';
+
+	}
+
+	changeBirdMove(position){
+		this.elem.style.backgroundPosition=`0px -${this.height*position}px`;
 	}
 }
 
 class Pipe{
 	constructor(min,max){
+		console.log(min,CONTAINER_HEIGHT-GAP-max);
 		this.y=getRandomInt(min,CONTAINER_HEIGHT-GAP-max);
+		this.width=20;
 
 		this.draw();
 	}
 
 	draw(){
 		this.elemTop=document.createElement('div');
-		this.elemTop.style.background='green';
+		// this.elemTop.classList.add('top');
+		this.elemTop.style.background='url(pipe-down.png)';
 		this.elemTop.style.position='absolute';
 		this.elemTop.style.top='0px';
 		this.elemTop.style.height=this.y+'px';
-		this.elemTop.style.width='20px';
+		this.elemTop.style.width=this.width+'px';
 		this.elemTop.style.left='1300px';
 
 		this.elemBottom=document.createElement('div');
-		this.elemBottom.style.background='green';
+		this.elemBottom.style.background='url(pipe-down.png)';
 		this.elemBottom.style.position='absolute';
 		this.elemBottom.style.top=this.y+GAP+'px';
 		this.elemBottom.style.height=CONTAINER_HEIGHT-(this.y+GAP)+'px';
 		this.elemBottom.style.width='20px';
 		this.elemBottom.style.left='1300px';
+		this.elemBottom.classList.add('bottom');
 
 		document.getElementById('container').appendChild(this.elemTop);
 		document.getElementById('container').appendChild(this.elemBottom);
@@ -71,8 +79,11 @@ class Pipe{
 	update(){
 		let left=parseInt(this.elemTop.style.left);
 
+		// console.log(this.elemTop.style.marginLeft,left);
+
 		this.elemTop.style.left=left-1+'px';
 		this.elemBottom.style.left=left-1+'px';
+
 	}
 
 	getY(){
@@ -98,7 +109,25 @@ class Game{
 	}
 
 	init(){
+		this.gameStart=false;
+		this.gameOver=false;
+
+		document.addEventListener('keydown', (event) => {
+			if(!gameTHAT.gameOver && !gameTHAT.gameStart && event.keyCode==32){
+				
+				gameTHAT.gameStart=true;
+				gameTHAT.start();	
+
+				gameTHAT.msgBoard.style.display='none';
+			}
+		});
+
+		this.msgBoard=document.getElementById('msgBoard');
+
 		
+	}
+
+	start(){
 		let tick=0;
 		let curMin=0,curMax=20;
 		
@@ -107,39 +136,41 @@ class Game{
 		let scoreBoard=document.getElementById('scoreBoard');
 
 		document.addEventListener('keydown', (event) => {
-			if(event.keyCode==32){
+			if(gameTHAT.gameStart && event.keyCode==32){
 				gameTHAT.bird.down=-1;
-				// console.log('SPace');
 				isBirdUp=1;	
 			}
 		});
 		
 		let pipePointer=0;
 
-		let score=0;
+		this.score=0;
 
-		gameInterval=setInterval(function(){
+		let birdMovePointer=0;
+
+		this.gameInterval=setInterval(function(){
 			for(let i=0;i<gameTHAT.pipes.length;i++){
 				gameTHAT.pipes[i].update();
 
 			}
 
+			//Check collision with pipes;
 			if(gameTHAT.pipes.length>pipePointer
 				&& (gameTHAT.bird.x+gameTHAT.bird.width>= gameTHAT.pipes[pipePointer].getX() 
-					&& gameTHAT.bird.x+gameTHAT.bird.width<=gameTHAT.pipes[pipePointer].getX()+20) 
+					&& gameTHAT.bird.x<=gameTHAT.pipes[pipePointer].getX()+gameTHAT.pipes[pipePointer].width) 
 				&& (gameTHAT.bird.y<=gameTHAT.pipes[pipePointer].y 
 					|| gameTHAT.bird.y+gameTHAT.bird.height>=gameTHAT.pipes[pipePointer].y+GAP)){
-				// console.log('I am here');
-				clearInterval(gameInterval);
+				
+				gameTHAT.stop();
 			}
 
 			if(gameTHAT.pipes.length>pipePointer && gameTHAT.pipes[pipePointer].getX()+20<gameTHAT.bird.x){
 				
 				pipePointer++;
 
-				score++;
-				scoreBoard.innerHTML=score;
-				console.log(score);
+				gameTHAT.score++;
+				scoreBoard.innerHTML=gameTHAT.score;
+				// console.log(score);
 			}
 			if(gameTHAT.pipes.length>0 && gameTHAT.pipes[0].getX()+20<0){
 
@@ -148,25 +179,39 @@ class Game{
 
 				pipePointer--;
 			}
+			
+			//Checks when to insert new pipe
 			tick++;
 			if(tick>=200){
 				tick=0;
 
 				let p=new Pipe(curMin,curMax);
 				gameTHAT.pipes.push(p);
-				curMin=p.getY()-100;
-				curMax=Math.max(curMin,0);
 
-				curMax=p.getY()+GAP+100;
+				//New Range for pipe to generate
+				curMin=p.getY()-100;
+				curMin=Math.max(curMin,0);
+
+				curMax=p.getY()+200;
 				curMax=Math.min(curMax,CONTAINER_HEIGHT-GAP-50);
 			}
 
-			if(gameTHAT.bird.y+gameTHAT.bird.height<CONTAINER_HEIGHT){
+			//Change sprite of bird movement
+			if(tick%10==0){
+				gameTHAT.bird.changeBirdMove(birdMovePointer);
 
-				gameTHAT.bird.update();	
+				birdMovePointer=(birdMovePointer+1)%4;
+			}
+
+			//Check collision with container else update bird position
+			if(gameTHAT.bird.y+gameTHAT.bird.height>=CONTAINER_HEIGHT
+				|| gameTHAT.bird.y<0){
+				
+				gameTHAT.stop();
 			}
 			else{
-				clearInterval(gameInterval);
+
+				gameTHAT.bird.update();
 			}
 
 			if(isBirdUp){
@@ -180,6 +225,16 @@ class Game{
 
 		},10);
 	}
+
+	stop(){
+		this.gameStart=false;
+		this.gameOver=true;
+		clearInterval(this.gameInterval);
+
+		this.msgBoard.children[0].innerHTML='Game Over';
+		this.msgBoard.children[1].innerHTML=`Your score is ${this.score}`;
+		this.msgBoard.style.display='block';
+	}
 }
 
 function getRandomInt(min, max) {
@@ -188,5 +243,4 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-var gameInterval;
 (new Game()).init();
